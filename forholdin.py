4e8599ac45a34d749be0a050359f08c2cbfe1652
jasmineandtina <jiange_li@msn.com>
2019-05-03 10:19:44 +0800

    调通数据csv文件1

--- forholdin.py
+++ forholdin.py
@@ -1,7 +1,9 @@
 from pandas import read_csv
-
+from pandas import set_option
 filename='holdin/20190222.csv'
 # names={'code','hold'}
 data=read_csv(filename)  #,names)
-
-print(data.head(20))
+set_option('display.width',50)
+print(data.head(200))
+print(data.describe())
+print(data.skew())
